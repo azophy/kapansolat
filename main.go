92b35eb915163793d8278c329553dde8e2e94501
Abdurrahman Shofy Adianto <azophy@gmail.com>
2024-07-30 17:37:23 +0700

    chore: go fmt

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-  "log"
-  "fmt"
-  "time"
-  "errors"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
-  "io/ioutil"
-  "encoding/json"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -15,56 +15,56 @@ import (
 var APP_PORT = "3000"
 
 func PrayerNames() []string {
-  return []string{"Fajr","Dhuhr", "Asr", "Maghrib", "Isha"}
+	return []string{"Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"}
 }
 
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-    ipAddr := c.RealIP()
+		ipAddr := c.RealIP()
 
-    loc, err := getIpInfo(ipAddr)
-    if err != nil {
-      return err
-    }
+		loc, err := getIpInfo(ipAddr)
+		if err != nil {
+			return err
+		}
 
-    locTz, _ := time.LoadLocation(loc.Timezone)
-    curTime := time.Now().In(locTz)
+		locTz, _ := time.LoadLocation(loc.Timezone)
+		curTime := time.Now().In(locTz)
 
-    prayerTimes, err := getPrayerTimes(curTime.Format("02-01-2006"), loc)
-    if err != nil {
-      return err
-    }
+		prayerTimes, err := getPrayerTimes(curTime.Format("02-01-2006"), loc)
+		if err != nil {
+			return err
+		}
 
-    nextPrayer, nextPrayerUntil, err := getPrayerTimeCountdown(curTime, loc, prayerTimes)
-    if err != nil {
-      return err
-    }
+		nextPrayer, nextPrayerUntil, err := getPrayerTimeCountdown(curTime, loc, prayerTimes)
+		if err != nil {
+			return err
+		}
 
-    contentType := c.Response().Header().Get(echo.HeaderContentType)
-    acceptType := c.Response().Header().Get(echo.HeaderAccept)
-    if acceptType == "application/json" || contentType == "application/json" {
-      return responseJson(c, curTime, loc, prayerTimes, nextPrayer, nextPrayerUntil)
-    }
+		contentType := c.Response().Header().Get(echo.HeaderContentType)
+		acceptType := c.Response().Header().Get(echo.HeaderAccept)
+		if acceptType == "application/json" || contentType == "application/json" {
+			return responseJson(c, curTime, loc, prayerTimes, nextPrayer, nextPrayerUntil)
+		}
 
-    return responsePlaintext(c, curTime, loc, prayerTimes, nextPrayer, nextPrayerUntil)
+		return responsePlaintext(c, curTime, loc, prayerTimes, nextPrayer, nextPrayerUntil)
 	})
 
-  // https://echo.labstack.com/docs/error-handling
-  // e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
-  //   return func(c echo.Context) error {
-  //     // https://sorcererxw.com/en/articles/go-echo-error-handing
-  //     err := next(c)
-  //     log.Printf("encounter error %v", err)
-  //     return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
-  //   }
-  // })
+	// https://echo.labstack.com/docs/error-handling
+	// e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+	//   return func(c echo.Context) error {
+	//     // https://sorcererxw.com/en/articles/go-echo-error-handing
+	//     err := next(c)
+	//     log.Printf("encounter error %v", err)
+	//     return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	//   }
+	// })
 
 	e.Logger.Fatal(e.Start(":" + APP_PORT))
 }
 
 func responsePlaintext(c echo.Context, curTime time.Time, loc IpInfo, prayerTimes PrayerTimes, nextPrayer string, nextPrayerUntil time.Duration) error {
-  respText := fmt.Sprintf(`KapanSolat
+	respText := fmt.Sprintf(`KapanSolat
 ==========
 detected location: %v, %v, %v
 current local time: %v
@@ -73,61 +73,61 @@ next prayer: %v (%v remaining)
 prayer times for %v
 %v`, loc.City, loc.Region, loc.Country, curTime.Format("15:04"), nextPrayer, nextPrayerUntil.Round(time.Minute).String(), curTime.Format("02-01-2006"), prayerTimes)
 
-  return c.String(http.StatusOK, respText)
+	return c.String(http.StatusOK, respText)
 }
 
 func responseJson(c echo.Context, curTime time.Time, loc IpInfo, prayerTimes PrayerTimes, nextPrayer string, nextPrayerUntil time.Duration) error {
-		return c.JSON(http.StatusOK, echo.Map{
-			"current_location": loc,
-      "current_time": curTime,
-      "prayer_times": prayerTimes,
-      "next_prayer": nextPrayer,
-      "time_to_next_prayer": nextPrayerUntil.Round(time.Minute).String(),
-		})
+	return c.JSON(http.StatusOK, echo.Map{
+		"current_location":    loc,
+		"current_time":        curTime,
+		"prayer_times":        prayerTimes,
+		"next_prayer":         nextPrayer,
+		"time_to_next_prayer": nextPrayerUntil.Round(time.Minute).String(),
+	})
 
 }
 
 func jsonRequest(req *http.Request, res interface{}) error {
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-    return err
+		return err
 	}
 
-  if resp.StatusCode != 200 {
-    return errors.New(fmt.Sprintf("http response unsuccessful. got status code %v", resp.StatusCode))
-  }
-
-  respBody, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    return err
-	}
-  log.Printf(string(respBody))
-
-  err = json.Unmarshal(respBody, &res)
-  if err != nil {
-    return err
+	if resp.StatusCode != 200 {
+		return errors.New(fmt.Sprintf("http response unsuccessful. got status code %v", resp.StatusCode))
 	}
 
-  return nil
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	log.Printf(string(respBody))
+
+	err = json.Unmarshal(respBody, &res)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type IpInfo struct {
-  Addr     string `json:"query"`
+	Addr     string  `json:"query"`
 	Lat      float64 `json:"lat"`
 	Lon      float64 `json:"lon"`
-	Country  string `json:"country"`
-	Region   string `json:"regionName"`
-	City     string `json:"city"`
-	Timezone string `json:"timezone"`
+	Country  string  `json:"country"`
+	Region   string  `json:"regionName"`
+	City     string  `json:"city"`
+	Timezone string  `json:"timezone"`
 }
 
 func getIpInfo(ipAddr string) (IpInfo, error) {
 	var res IpInfo
-  url := "http://ip-api.com/json/" + ipAddr
+	url := "http://ip-api.com/json/" + ipAddr
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-    return res, err
+		return res, err
 	}
 
 	// req.Header = http.Header{
@@ -138,92 +138,93 @@ func getIpInfo(ipAddr string) (IpInfo, error) {
 
 	err = jsonRequest(req, &res)
 	if err != nil {
-    return res, err
+		return res, err
 	}
 
 	return res, nil
 }
 
-//type PrayerTimes struct {
-  //Location IpInfo
-  //Data struct {
-    //Timings struct {
-      //Fajr     string `json:"Fajr"`
-      //Dhuhr    string `json:"Dhuhr"`
-      //Asr      string `json:"Asr"`
-      //Maghrib  string `json:"Maghrib"`
-      //Isha     string `json:"Isha"`
-    //} `json:"timings"`
-  //} `json:"data"`
-//}
 // type PrayerTimes struct {
-//   Location IpInfo
-//   Fajr     string `json:"Fajr"`
-//   Dhuhr    string `json:"Dhuhr"`
-//   Asr      string `json:"Asr"`
-//   Maghrib  string `json:"Maghrib"`
-//   Isha     string `json:"Isha"`
+// Location IpInfo
+// Data struct {
+// Timings struct {
+// Fajr     string `json:"Fajr"`
+// Dhuhr    string `json:"Dhuhr"`
+// Asr      string `json:"Asr"`
+// Maghrib  string `json:"Maghrib"`
+// Isha     string `json:"Isha"`
+// } `json:"timings"`
+// } `json:"data"`
 // }
+//
+//	type PrayerTimes struct {
+//	  Location IpInfo
+//	  Fajr     string `json:"Fajr"`
+//	  Dhuhr    string `json:"Dhuhr"`
+//	  Asr      string `json:"Asr"`
+//	  Maghrib  string `json:"Maghrib"`
+//	  Isha     string `json:"Isha"`
+//	}
 type PrayerTimes map[string]string
 
 func getPrayerTimes(date string, location IpInfo) (PrayerTimes, error) {
-  res := make(PrayerTimes)
-  url := fmt.Sprintf("http://api.aladhan.com/v1/timings/%s?method=20&latitude=%v&longitude=%v", date, location.Lat, location.Lon)
+	res := make(PrayerTimes)
+	url := fmt.Sprintf("http://api.aladhan.com/v1/timings/%s?method=20&latitude=%v&longitude=%v", date, location.Lat, location.Lon)
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-    return res, err
+		return res, err
 	}
 
-  var resp interface{}
+	var resp interface{}
 	err = jsonRequest(req, &resp)
 	if err != nil {
-    return res, err
+		return res, err
 	}
 
-  timings := resp.(map[string]interface{})["data"].(map[string]interface{})["timings"].(map[string]interface{})
+	timings := resp.(map[string]interface{})["data"].(map[string]interface{})["timings"].(map[string]interface{})
 
-  for _,i := range PrayerNames() {
-    res[i] = timings[i].(string)
-  }
-  // res = PrayerTimes{
-  //   Location: location,
-  //   Fajr: timings["Fajr"].(string),
-  //   Dhuhr: timings["Dhuhr"].(string),
-  //   Asr: timings["Asr"].(string),
-  //   Maghrib: timings["Maghrib"].(string),
-  //   Isha: timings["Isha"].(string),
-  // }
+	for _, i := range PrayerNames() {
+		res[i] = timings[i].(string)
+	}
+	// res = PrayerTimes{
+	//   Location: location,
+	//   Fajr: timings["Fajr"].(string),
+	//   Dhuhr: timings["Dhuhr"].(string),
+	//   Asr: timings["Asr"].(string),
+	//   Maghrib: timings["Maghrib"].(string),
+	//   Isha: timings["Isha"].(string),
+	// }
 
 	return res, nil
 }
 
 func parseTime(datetime, loc string) (time.Time, error) {
-  var emptyTime time.Time
-  const format = "02-01-2006 15:04"
-  tz, err := time.LoadLocation(loc)
-  if err != nil {
-    return emptyTime, err
-  }
-  return time.ParseInLocation(format, datetime, tz)
+	var emptyTime time.Time
+	const format = "02-01-2006 15:04"
+	tz, err := time.LoadLocation(loc)
+	if err != nil {
+		return emptyTime, err
+	}
+	return time.ParseInLocation(format, datetime, tz)
 }
 
 func getPrayerTimeCountdown(curTime time.Time, loc IpInfo, prayerTimes PrayerTimes) (string, time.Duration, error) {
-  date := curTime.Format("02-01-2006 ")
-  nextPrayer := ""
-  var duration time.Duration
+	date := curTime.Format("02-01-2006 ")
+	nextPrayer := ""
+	var duration time.Duration
 
-  for _,i := range PrayerNames() {
-    parsedTime, err := parseTime(date + prayerTimes[i], loc.Timezone)
-    if err != nil {
-      return nextPrayer, duration, err
-    }
+	for _, i := range PrayerNames() {
+		parsedTime, err := parseTime(date+prayerTimes[i], loc.Timezone)
+		if err != nil {
+			return nextPrayer, duration, err
+		}
 
-    if curTime.Before(parsedTime) {
-      nextPrayer = i
-      duration = parsedTime.Sub(curTime)
-      break
-    }
-  }
-  return nextPrayer, duration, nil
+		if curTime.Before(parsedTime) {
+			nextPrayer = i
+			duration = parsedTime.Sub(curTime)
+			break
+		}
+	}
+	return nextPrayer, duration, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "log"
-  "testing"
+	"log"
+	"testing"
 )
 
 const (
@@ -18,95 +18,95 @@ const (
 
 func TestGetIpInfoKemenag(t *testing.T) {
 	res, _ := getIpInfo(ipKemenag)
-  log.Printf("result: %v\n", res)
-	if res.Country != "Indonesia"  {
+	log.Printf("result: %v\n", res)
+	if res.Country != "Indonesia" {
 		t.Errorf("expecting country Indonesia, got %v", res.Country)
 	}
 }
 
 func TestGetPrayerTime(t *testing.T) {
-  // result from above getIp query
-  location := IpInfo{
-    Addr: "103.7.13.24",
-    Country: "Indonesia",
-    Region: "Jakarta",
-    City: "Jakarta",
-    Lat: -6.17189,
-    Lon: 106.834,
-    Timezone: "Asia/Jakarta",
-  }
+	// result from above getIp query
+	location := IpInfo{
+		Addr:     "103.7.13.24",
+		Country:  "Indonesia",
+		Region:   "Jakarta",
+		City:     "Jakarta",
+		Lat:      -6.17189,
+		Lon:      106.834,
+		Timezone: "Asia/Jakarta",
+	}
 
 	res, _ := getPrayerTimes("14-07-2024", location)
-  log.Printf("result: %v\n", res)
-  if res["Isha"] != "19:06"  {
-    t.Errorf("isha time result doesn't match")
-  }
+	log.Printf("result: %v\n", res)
+	if res["Isha"] != "19:06" {
+		t.Errorf("isha time result doesn't match")
+	}
 }
 
 func TestParseTime(t *testing.T) {
-  res, err := parseTime("14-07-2024 19:06", "Asia/Jakarta")
-  if err != nil {
-    t.Errorf("encounter error %v", err)
-  }
+	res, err := parseTime("14-07-2024 19:06", "Asia/Jakarta")
+	if err != nil {
+		t.Errorf("encounter error %v", err)
+	}
 
-  h,m,_ := res.Clock()
-  if h != 19 || m != 6  {
-    t.Error("parse result incorrect")
-  }
+	h, m, _ := res.Clock()
+	if h != 19 || m != 6 {
+		t.Error("parse result incorrect")
+	}
 }
 
 type testCase struct {
-  curTime string
-  nextPrayer string
-  durMinutes int64
+	curTime    string
+	nextPrayer string
+	durMinutes int64
 }
 
 func TestGetPrayerTimeCountdown(t *testing.T) {
-  loc := IpInfo{
-    Addr: "103.7.13.24",
-    Country: "Indonesia",
-    Region: "Jakarta",
-    City: "Jakarta",
-    Lat: -6.17189,
-    Lon: 106.834,
-    Timezone: "Asia/Jakarta",
-  }
-  prayerTimes := PrayerTimes{
-    "Fajr": "04:00",
-    "Dhuhr": "12:00",
-    "Asr": "15:00",
-    "Maghrib": "18:00",
-    "Isha": "20:00",
-  }
-  testCases := []testCase{
-    {
-      curTime: "14-07-2024 03:00",
-      nextPrayer: "Fajr",
-      durMinutes: 60,
-    },
-    {
-      curTime: "14-07-2024 12:00",
-      nextPrayer: "Asr",
-      durMinutes: 180,
-    },
-    {
-      curTime: "14-07-2024 23:00",
-      nextPrayer: "",
-      durMinutes: 0,
-    },
-  }
+	loc := IpInfo{
+		Addr:     "103.7.13.24",
+		Country:  "Indonesia",
+		Region:   "Jakarta",
+		City:     "Jakarta",
+		Lat:      -6.17189,
+		Lon:      106.834,
+		Timezone: "Asia/Jakarta",
+	}
+	prayerTimes := PrayerTimes{
+		"Fajr":    "04:00",
+		"Dhuhr":   "12:00",
+		"Asr":     "15:00",
+		"Maghrib": "18:00",
+		"Isha":    "20:00",
+	}
+	testCases := []testCase{
+		{
+			curTime:    "14-07-2024 03:00",
+			nextPrayer: "Fajr",
+			durMinutes: 60,
+		},
+		{
+			curTime:    "14-07-2024 12:00",
+			nextPrayer: "Asr",
+			durMinutes: 180,
+		},
+		{
+			curTime:    "14-07-2024 23:00",
+			nextPrayer: "",
+			durMinutes: 0,
+		},
+	}
 
-  for _, item := range testCases {
-    curTime, _ := parseTime(item.curTime, "Asia/Jakarta")
+	for _, item := range testCases {
+		curTime, _ := parseTime(item.curTime, "Asia/Jakarta")
 
-    nextPrayer, timeRemaining, err := getPrayerTimeCountdown(curTime, loc, prayerTimes)
-    if err != nil {
-      t.Errorf("encounter error %v", err)
-    }
+		nextPrayer, timeRemaining, err := getPrayerTimeCountdown(curTime, loc, prayerTimes)
+		if err != nil {
+			t.Errorf("encounter error %v", err)
+		}
 
-    remainingMinutes := int64(timeRemaining.Minutes())
-    if nextPrayer != item.nextPrayer|| remainingMinutes != item.durMinutes {
-      t.Errorf("parse result incorrect, expecting %v %v,got %v %v", item.nextPrayer, item.durMinutes, nextPrayer, remainingMinutes)
-    }
-  }
+		remainingMinutes := int64(timeRemaining.Minutes())
+		if nextPrayer != item.nextPrayer || remainingMinutes != item.durMinutes {
+			t.Errorf("parse result incorrect, expecting %v %v,got %v %v", item.nextPrayer, item.durMinutes, nextPrayer, remainingMinutes)
+		}
+	}
 }
